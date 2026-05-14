2ade1d81f5c115832b903221721fd24f834355e7
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-05-14 12:12:53 -0400

    fix: disable pylance checking

--- 1-arrays-and-hashing/217-Contains-Duplicate/217-Contains-Duplicate.py
+++ 1-arrays-and-hashing/217-Contains-Duplicate/217-Contains-Duplicate.py
@@ -7,8 +7,6 @@ revisit_in_days = 3
 difficulty      = "easy"
 topic_tags      = ["arrays", "hash-set"]
 
-from typing import List
-
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return not len(set(nums)) == len(nums)
