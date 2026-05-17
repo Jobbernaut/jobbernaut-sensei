6609a2e3845808af9ee3a9972d5cb5a5014c6f99
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-05-16 21:00:55 -0400

    feat: 05/16/26 update

--- 1-arrays-and-hashing/271-Encode-And-Decode-Strings/271-Encode-And-Decode-Strings.py
+++ 1-arrays-and-hashing/271-Encode-And-Decode-Strings/271-Encode-And-Decode-Strings.py
@@ -2,8 +2,8 @@
 https://leetcode.com/problems/encode-and-decode-strings/description/
 '''
 
-last_solved     = "2026-05-05"
-revisit_in_days = 7
+last_solved     = "2026-05-16"
+revisit_in_days = 30
 difficulty      = "medium"
 topic_tags      = ["arrays", "hashing"]
 
