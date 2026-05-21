51e7b4310b9a818f536d5b29cc6cf5af9f3c46d3
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-05-21 10:40:31 -0400

    feat: solve 230 Kth smallest element

---
+++ 7-trees/230-Kth-Smallest-Element-In-A-BST/230-Kth-Smallest-Element-In-A-BST.py
@@ -0,0 +1,29 @@
+'''
+https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+'''
+
+last_solved     = "2026-05-21"
+revisit_in_days = 3
+difficulty      = "medium"
+topic_tags      = ["trees"]
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.count = k
+        self.result = None
+
+        def inorder(node):
+            if not node or self.result is not None:
+                return
+            
+            inorder(node.left)
+            
+            self.count -= 1
+            if self.count == 0:
+                self.result = node.val
+                return
+            
+            inorder(node.right)
+
+        inorder(root)
+        return self.result
