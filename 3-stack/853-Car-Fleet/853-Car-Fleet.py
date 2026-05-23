55f01d1e59410692ac488b8e8d7d89344d13f580
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-05-23 16:01:42 -0400

    feat: 853 revision

--- 3-stack/853-Car-Fleet/853-Car-Fleet.py
+++ 3-stack/853-Car-Fleet/853-Car-Fleet.py
@@ -2,19 +2,23 @@
 https://leetcode.com/problems/car-fleet/description/
 '''
 
-last_solved     = "2026-05-05"
-revisit_in_days = 3
+last_solved     = "2026-05-23"
+revisit_in_days = 7
 difficulty      = "medium"
 topic_tags      = ["stack", "monotonic-stack"]
 
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pairs = sorted(zip(position, speed), reverse=True)
         stack = []
 
-        for pos, spd in pairs:
-            time = (target - pos) / spd
-            if not stack or time > stack[-1]:
-                stack.append(time)
+        pos_spd_sorted = sorted(list(zip(position, speed)), reverse=True)
+
+        for pos, spd in pos_spd_sorted:
+            arrival_time = (target - pos) / spd
+
+            if stack and arrival_time <= stack[-1]:
+                continue
+            
+            stack.append(arrival_time)
 
         return len(stack)
