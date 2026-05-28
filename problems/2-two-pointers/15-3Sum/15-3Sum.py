39e54333ff0cecb80bf25651f04450ebbe20214e
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-05-28 10:52:52 -0400

    feat: revisit three sum

---
+++ problems/2-two-pointers/15-3Sum/15-3Sum.py
@@ -0,0 +1,36 @@
+'''
+https://leetcode.com/problems/3sum/
+'''
+
+last_solved     = "2026-05-28"
+revisit_in_days = 3
+difficulty      = "medium"
+topic_tags      = ["two-pointers", "array"]
+times_reviewed  = 0
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        output = []
+
+        nums.sort()
+        for idx, num in enumerate(nums):
+            if idx > 0 and nums[idx] == nums[idx - 1]:
+                continue
+            target = -num
+            left, right = idx + 1, len(nums) - 1
+            while left < right:
+                local = nums[left] + nums[right]
+                if local == target:
+                    output.append([nums[idx], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif local < target:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return output
