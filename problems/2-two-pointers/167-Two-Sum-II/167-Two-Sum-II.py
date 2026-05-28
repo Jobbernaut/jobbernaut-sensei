abe0ffe02834ffa9ba0de477637452f4f582c53d
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-05-28 10:51:07 -0400

    feat: revisit two sum ii

---
+++ problems/2-two-pointers/167-Two-Sum-II/167-Two-Sum-II.py
@@ -0,0 +1,23 @@
+'''
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+'''
+
+last_solved     = "2026-05-28"
+revisit_in_days = 90
+difficulty      = "medium"
+topic_tags      = ["two-pointers", "array"]
+times_reviewed  = 1
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+
+        while left < right:
+            local = numbers[left] + numbers[right]
+
+            if local == target:
+                return [left + 1, right + 1]
+            elif local < target:
+                left += 1
+            else:
+                right -= 1
