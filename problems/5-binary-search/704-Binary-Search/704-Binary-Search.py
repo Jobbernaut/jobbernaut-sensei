7f57e1e10233477ba5d1f3037134d20bc5956391
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-05-29 10:43:59 -0400

    feat: revisit 704 Binary Search

---
+++ problems/5-binary-search/704-Binary-Search/704-Binary-Search.py
@@ -0,0 +1,25 @@
+'''
+https://leetcode.com/problems/binary-search/
+'''
+
+last_solved     = "2026-05-29"
+revisit_in_days = 90
+difficulty      = "easy"
+topic_tags      = ["binary-search"]
+times_reviewed  = 0
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return -1
