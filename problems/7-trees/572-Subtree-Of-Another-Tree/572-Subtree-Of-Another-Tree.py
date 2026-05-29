f3604cc7fb232cf0248a83a4894c63b2a49e67ec
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-05-29 11:29:26 -0400

    feat: solve 572 subtree of another tree

---
+++ problems/7-trees/572-Subtree-Of-Another-Tree/572-Subtree-Of-Another-Tree.py
@@ -0,0 +1,29 @@
+'''
+https://leetcode.com/problems/subtree-of-another-tree/
+'''
+
+last_solved     = "2026-05-29"
+revisit_in_days = 3
+difficulty      = "easy"
+topic_tags      = ["trees"]
+times_reviewed  = 0
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def is_same(node1, node2):
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+            return is_same(node1.left, node2.left) and is_same(node1.right, node2.right)
+
+        def traverse(node):
+            if not node:
+                return False
+            
+            if is_same(node, subRoot):
+                return True
+
+            return traverse(node.left) or traverse(node.right)
+
+        return traverse(root)
