9dc574c65ef92103299a49791447e4511b99084f
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-05-29 11:11:28 -0400

    feat: revisit 110 balanced binary tree

---
+++ problems/7-trees/110-Balanced-Binary-Tree/110-Balanced-Binary-Tree.py
@@ -0,0 +1,29 @@
+'''
+https://leetcode.com/problems/balanced-binary-tree/
+'''
+
+last_solved     = "2026-05-29"
+revisit_in_days = 90
+difficulty      = "easy"
+topic_tags      = ["trees"]
+times_reviewed  = 1
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.balanced = True
+
+        def depth(node):
+            if not node:
+                return 0
+            
+            left = depth(node.left)
+            right = depth(node.right)
+
+            if abs(left - right) > 1:
+                self.balanced = False
+            
+            return 1 + max(left, right)
+        
+        depth(root)
+
+        return self.balanced
