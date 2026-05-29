64011b2b84d8681448fcec5829e4a99171525c5a
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-05-29 11:03:10 -0400

    feat: revisit 104 max depth

---
+++ problems/7-trees/104-Maximum-Depth-Of-Binary-Tree/104-Maximum-Depth-Of-Binary-Tree.py
@@ -0,0 +1,23 @@
+'''
+https://leetcode.com/problems/maximum-depth-of-binary-tree/
+'''
+
+last_solved     = "2026-05-29"
+revisit_in_days = 90
+difficulty      = "easy"
+topic_tags      = ["trees"]
+times_reviewed  = 1
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def depth(node):
+            if not node:
+                return 0
+            elif not node.left and node.right:
+                return 1 + depth(node.right)
+            elif node.left and not node.right:
+                return 1 + depth(node.left)
+            else:
+                return 1 + max(depth(node.left), depth(node.right))
+        
+        return depth(root)
