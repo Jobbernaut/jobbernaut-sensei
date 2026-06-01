2bcbc33e972f51458f0fedf2ca0e5bae20470afe
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-01 14:18:47 -0400

    revisit: 66

--- problems/17-math-and-geometry/66-Plus-One/66-Plus-One.py
+++ problems/17-math-and-geometry/66-Plus-One/66-Plus-One.py
@@ -2,28 +2,22 @@
 https://leetcode.com/problems/plus-one/
 '''
 
-last_solved     = "2026-05-29"
-revisit_in_days = 3
+last_solved     = "2026-06-01"
+revisit_in_days = 90
 difficulty      = "easy"
 topic_tags      = ["math"]
-times_reviewed  = 0
+times_reviewed  = 1
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        carry = 0
-
         for idx in range(len(digits) - 1, -1, -1):
             if digits[idx] == 9:
                 digits[idx] = 0
-                carry = 1
             else:
                 digits[idx] += 1
-                carry = 0
-                break
-            if not carry:
                 break
         
-        if not carry:
-            return digits
-        else:
+        if digits[0] == 0:
             return [1] + digits
+        
+        return digits
