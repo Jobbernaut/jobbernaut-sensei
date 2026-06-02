08d66a165fd8cdccfbaf88dcbdff6aa69bc8d40a
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-02 16:05:10 -0400

    new: 567

---
+++ problems/3-sliding-window/567-Permutation-In-String/567-Permutation-In-String.py
@@ -0,0 +1,32 @@
+'''
+https://leetcode.com/problems/permutation-in-string/
+'''
+
+last_solved     = "2026-06-02"
+revisit_in_days = 3
+difficulty      = "medium"
+topic_tags      = ["sliding-window", "hash-map"]
+times_reviewed  = 1
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_sig = [0] * 26
+        s2_sig = [0] * 26
+
+        for c in s1:
+            s1_sig[ord(c) - ord('a')] += 1
+        
+        left = right = 0
+        while left <= right and right < len(s2):
+            s2_sig[ord(s2[right]) - ord('a')] += 1
+
+            if right - left + 1 > len(s1):
+                s2_sig[ord(s2[left]) - ord('a')] -= 1
+                left += 1
+            if right - left + 1 == len(s1):
+                if s1_sig == s2_sig:
+                    return True
+
+            right += 1
+        
+        return False
