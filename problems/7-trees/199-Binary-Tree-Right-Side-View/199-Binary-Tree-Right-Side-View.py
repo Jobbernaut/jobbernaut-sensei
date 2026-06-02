b61cf3d914cab3019412c7efd1e7bcc23217d338
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-02 15:31:20 -0400

    new: 199

---
+++ problems/7-trees/199-Binary-Tree-Right-Side-View/199-Binary-Tree-Right-Side-View.py
@@ -0,0 +1,31 @@
+'''
+https://leetcode.com/problems/binary-tree-right-side-view/
+'''
+
+last_solved     = "2026-06-02"
+revisit_in_days = 3
+difficulty      = "medium"
+topic_tags      = ["trees", "bfs"]
+times_reviewed  = 1
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        output = []
+
+        if not root:
+            return output
+        
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            rightmost = None
+            for _ in range(len(queue)):
+                rightmost = queue.popleft()
+                if rightmost.left:
+                    queue.append(rightmost.left)
+                if rightmost.right:
+                    queue.append(rightmost.right)
+            output.append(rightmost.val)
+        
+        return output
