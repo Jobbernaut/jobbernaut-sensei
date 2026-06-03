e83828241a8f437a3322a8551b093662dfcc8069
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-03 13:20:50 -0400

    revisit: 1

--- problems/1-arrays-and-hashing/1-Two-Sum/1-Two-Sum.py
+++ problems/1-arrays-and-hashing/1-Two-Sum/1-Two-Sum.py
@@ -2,23 +2,16 @@
 https://leetcode.com/problems/two-sum/description/
 '''
 
-last_solved     = "2026-05-04"
-revisit_in_days = 30
+last_solved     = "2026-06-03"
+revisit_in_days = 3
 difficulty      = "easy"
 topic_tags      = ["arrays", "hashing"]
+times_reviewed  = 1
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = defaultdict(list)
-
+        hashmap = {}
         for idx, num in enumerate(nums):
-            d[num].append(idx)
-
-            if target - num in d:
-                if num == target - num:
-                    if len(d[target - num]) > 1:
-                        return d[target - num]
-                    else:
-                        continue
-                else:
-                    return [d[target - num][0], idx]
+            if target - num in hashmap:
+                return [idx, hashmap[target - num]]
+            hashmap[num] = idx
