530cbb0098639e4bdcb793414cb0ef356604e19a
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-10 22:42:12 -0400

    new: 11

---
+++ problems/2-two-pointers/11-Container-With-Most-Water/11-Container-With-Most-Water.py
@@ -0,0 +1,23 @@
+'''
+https://leetcode.com/problems/container-with-most-water/
+'''
+
+last_solved     = "2026-06-10"
+revisit_in_days = 7
+difficulty      = "medium"
+topic_tags      = ["two-pointers"]
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        maxi = float('-inf')
+
+        while left < right:
+            maxi = max(maxi, min(height[left], height[right]) * (right - left))
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return maxi
