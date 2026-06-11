9188c5449d81d713e927fa021fc416691d608451
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-10 22:42:13 -0400

    new: 22

---
+++ problems/9-backtracking/22-Generate-Parentheses/22-Generate-Parentheses.py
@@ -0,0 +1,31 @@
+'''
+https://leetcode.com/problems/generate-parentheses/
+'''
+
+last_solved     = "2026-06-10"
+revisit_in_days = 7
+difficulty      = "medium"
+topic_tags      = ["backtracking", "recursion"]
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.output = []
+
+        def gen_par(open, close, par):
+            if open == n and close == n:
+                self.output.append("".join(par))
+                return
+            
+            if open < n:
+                par.append("(")
+                gen_par(open + 1, close, par)
+                par.pop()
+            
+            if close < open:
+                par.append(")")
+                gen_par(open, close + 1, par)
+                par.pop()
+
+        gen_par(0, 0, [])
+
+        return self.output
