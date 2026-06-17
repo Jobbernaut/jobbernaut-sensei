bc769fbc89911a792d94b1f01e70bf2b92db77db
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-17 15:45:23 -0400

    problems: new

---
+++ problems/7-trees/98-Validate-Binary-Search-Tree/98-Validate-Binary-Search-Tree.py
@@ -0,0 +1,21 @@
+'''
+https://leetcode.com/problems/validate-binary-search-tree/
+'''
+
+last_solved     = "2026-06-17"
+revisit_in_days = 3
+difficulty      = "medium"
+topic_tags      = ["trees", "bst"]
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def validate(node, lo, hi):
+            if not node:
+                return True
+
+            if lo < node.val < hi:
+                return validate(node.left, lo, node.val) and validate(node.right, node.val, hi)
+                
+            return False
+        
+        return validate(root, float('-inf'), float('inf'))
