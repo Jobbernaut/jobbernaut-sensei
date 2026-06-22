c84ae0dd779257d5ebc6ab6eeb27c0c2f5b56343
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-22 15:40:27 -0400

    solve: 215 Kth Largest Element in an Array

--- problems/8-heap/215-Kth-Largest-Element-in-an-Array/215-Kth-Largest-Element-in-an-Array.py
+++ problems/8-heap/215-Kth-Largest-Element-in-an-Array/215-Kth-Largest-Element-in-an-Array.py
@@ -2,20 +2,18 @@
 https://leetcode.com/problems/kth-largest-element-in-an-array/description/
 '''
 
-last_solved     = "2026-05-23"
+last_solved     = "2026-06-22"
 revisit_in_days = 30
 difficulty      = "medium"
 topic_tags      = ["heap"]
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        import heapq
-
         heap = []
-        heapq.heapify(heap)
 
         for num in nums:
             heapq.heappush(heap, num)
+
             if len(heap) > k:
                 heapq.heappop(heap)
         
