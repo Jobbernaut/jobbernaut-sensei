d397dc160d76c554c3387c6a44cc7f09b84e3492
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-21 21:23:19 -0400

    solve: 102 Binary Tree Level Order Traversal

--- problems/7-trees/102-Binary-Tree-Level-Order-Traversal/102-Binary-Tree-Level-Order-Traversal.py
+++ problems/7-trees/102-Binary-Tree-Level-Order-Traversal/102-Binary-Tree-Level-Order-Traversal.py
@@ -2,8 +2,8 @@
 https://leetcode.com/problems/binary-tree-level-order-traversal/
 '''
 
-last_solved     = "2026-06-13"
-revisit_in_days = 7
+last_solved     = "2026-06-21"
+revisit_in_days = 30
 difficulty      = "medium"
 topic_tags      = ["trees", "bfs"]
 times_reviewed  = 2
