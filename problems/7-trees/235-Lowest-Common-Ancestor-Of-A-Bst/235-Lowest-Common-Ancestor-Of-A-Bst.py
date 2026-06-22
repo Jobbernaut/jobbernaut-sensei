8a848c6e5165bf6cf7bef1560cf9e2b9d616c7cf
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-21 21:23:19 -0400

    solve: 235 Lowest Common Ancestor Of A BST

--- problems/7-trees/235-Lowest-Common-Ancestor-Of-A-Bst/235-Lowest-Common-Ancestor-Of-A-Bst.py
+++ problems/7-trees/235-Lowest-Common-Ancestor-Of-A-Bst/235-Lowest-Common-Ancestor-Of-A-Bst.py
@@ -2,8 +2,8 @@
 https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 '''
 
-last_solved     = "2026-06-13"
-revisit_in_days = 7
+last_solved     = "2026-06-21"
+revisit_in_days = 30
 difficulty      = "medium"
 topic_tags      = ["trees", "bst"]
 times_reviewed  = 2
