f065e84676e83b6538c7c36d7e06a834f250c24e
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-21 21:23:19 -0400

    solve: 567 Permutation In String

--- problems/3-sliding-window/567-Permutation-In-String/567-Permutation-In-String.py
+++ problems/3-sliding-window/567-Permutation-In-String/567-Permutation-In-String.py
@@ -2,8 +2,8 @@
 https://leetcode.com/problems/permutation-in-string/
 '''
 
-last_solved     = "2026-06-13"
-revisit_in_days = 7
+last_solved     = "2026-06-21"
+revisit_in_days = 30
 difficulty      = "medium"
 topic_tags      = ["sliding-window", "hash-map"]
 times_reviewed  = 2
