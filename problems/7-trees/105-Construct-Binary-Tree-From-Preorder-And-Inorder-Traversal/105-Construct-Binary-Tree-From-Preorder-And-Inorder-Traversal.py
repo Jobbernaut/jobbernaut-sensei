38b81457a3cabf7591f22b1114b48e73ad28cf3f
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-26 11:32:06 -0400

    solve: 105. Construct Binary Tree From Preorder And Inorder Traversal

--- problems/7-trees/105-Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal/105-Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal.py
+++ problems/7-trees/105-Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal/105-Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal.py
@@ -2,51 +2,49 @@
 https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 '''
 
-last_solved     = "2026-06-25"
-revisit_in_days = 1
+last_solved     = "2026-06-26"
+revisit_in_days = 3
 difficulty      = "medium"
 topic_tags      = ["trees"]
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        self.lookup = {}
+
+        for idx, node in enumerate(inorder):
+            self.lookup[node] = idx
         
-        self.inorder_lookup = {}
-        for idx, val in enumerate(inorder): self.inorder_lookup[val] = idx
-
-        def build(preorder_left, preorder_right, inorder_left, inorder_right):
-            if (
-                preorder_left > preorder_right or
-                preorder_left > len(preorder) - 1 or
-                preorder_right < 0
-                ):
+        def build(pre_left, pre_right, in_left, in_right):
+            if pre_left > pre_right:
                 return None
+            
+            root_node_val = preorder[pre_left]
+            in_root_idx = self.lookup[root_node_val]
 
-            root_preorder_val = preorder[preorder_left]
-            root_inorder_idx = self.inorder_lookup[root_preorder_val]
+            left_subtree_len = in_root_idx - in_left
+            right_subtree_len = in_right - in_root_idx
 
-            left_children_size = root_inorder_idx - inorder_left
+            node = TreeNode(root_node_val)
 
-            tree = TreeNode(root_preorder_val)
+            node.left = build(
+                pre_left + 1,
+                pre_left + left_subtree_len,
+                in_left,
+                in_root_idx - 1
+            )
 
-            tree.left = build(
-                preorder_left + 1,
-                preorder_left + left_children_size,
-                inorder_left,
-                root_inorder_idx - 1,
-                )
+            node.right = build(
+                pre_right - right_subtree_len + 1,
+                pre_right,
+                in_root_idx + 1,
+                in_right
+            )
 
-            tree.right = build(
-                preorder_left + left_children_size + 1,
-                preorder_right,
-                root_inorder_idx + 1,
-                inorder_right,
-                )
-
-            return tree
+            return node
         
         return build(
             0,
             len(preorder) - 1,
             0,
-            len(inorder) - 1,
-            )
+            len(inorder) - 1
+        )
