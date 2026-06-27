167c282ec41e832c54fccd8e78eaad4b9437e3d3
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-27 12:08:50 -0400

    feat(133): Clone Graph - BFS with original->clone hashmap

---
+++ problems/11-graphs/133-Clone-Graph/133-Clone-Graph.py
@@ -0,0 +1,42 @@
+'''
+https://leetcode.com/problems/clone-graph/
+'''
+
+last_solved     = "2026-06-27"
+revisit_in_days = 1
+difficulty      = "medium"
+topic_tags      = ["graphs", "dfs", "bfs", "hash-map"]
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return node
+
+        deep_copy_graph = Node(node.val)
+
+        orig = node
+        clone = deep_copy_graph
+
+        visited = {}
+        visited[orig] = clone
+
+        queue = deque()
+        queue.append((orig, clone))
+
+        while queue:
+            for _ in range(len(queue)):
+                curr_orig, curr_clone = queue.popleft()
+
+                for each_node in curr_orig.neighbors:
+                    new = Node()
+                
+                    new.val = each_node.val
+
+                    if each_node in visited:
+                        curr_clone.neighbors.append(visited[each_node])
+                    else:
+                        curr_clone.neighbors.append(new)
+                        visited[each_node] = new
+                        queue.append((each_node, new))
+        
+        return deep_copy_graph
