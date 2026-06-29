75aa0b4b46956728abf8e377af03fc808eed9e25
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-06-29 14:40:10 -0400

    review: 297 Serialize And Deserialize Binary Tree

--- problems/7-trees/297-Serialize-And-Deserialize-Binary-Tree/297-Serialize-And-Deserialize-Binary-Tree.py
+++ problems/7-trees/297-Serialize-And-Deserialize-Binary-Tree/297-Serialize-And-Deserialize-Binary-Tree.py
@@ -2,8 +2,8 @@
 https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
 '''
 
-last_solved     = "2026-06-26"
-revisit_in_days = 3
+last_solved     = "2026-06-29"
+revisit_in_days = 7
 difficulty      = "hard"
 topic_tags      = ["trees"]
 
