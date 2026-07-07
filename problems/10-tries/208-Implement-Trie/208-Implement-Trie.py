32f5fb58f27937d3f9e419891934435a0773a9c3
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-07-07 17:42:52 -0400

    review: 208. Implement Trie (medium) — rated e

--- problems/10-tries/208-Implement-Trie/208-Implement-Trie.py
+++ problems/10-tries/208-Implement-Trie/208-Implement-Trie.py
@@ -2,9 +2,9 @@
 https://leetcode.com/problems/implement-trie-prefix-tree/
 '''
 
-last_solved     = "2026-07-06"
-revisit_in_days = 1
-times_reviewed  = 5
+last_solved     = "2026-07-07"
+revisit_in_days = 45
+times_reviewed  = 6
 difficulty      = "medium"
 topic_tags      = ["trie"]
 
