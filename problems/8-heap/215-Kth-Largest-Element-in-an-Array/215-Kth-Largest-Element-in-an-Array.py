2c97027e0a2a076dcecc2cf900e8fc3f4bace481
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-07-22 15:54:18 -0400

    review: 215. Kth Largest Element in an Array [e]

--- problems/8-heap/215-Kth-Largest-Element-in-an-Array/215-Kth-Largest-Element-in-an-Array.py
+++ problems/8-heap/215-Kth-Largest-Element-in-an-Array/215-Kth-Largest-Element-in-an-Array.py
@@ -2,9 +2,9 @@
 https://leetcode.com/problems/kth-largest-element-in-an-array/description/
 '''
 
-last_solved     = "2026-06-22"
-revisit_in_days = 30
-times_reviewed  = 6
+last_solved     = "2026-07-22"
+revisit_in_days = 40
+times_reviewed  = 7
 difficulty      = "medium"
 topic_tags      = ["heap"]
 
