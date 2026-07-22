62b553a6b774f90dcfb09949dd36aaca8b9ab90a
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-07-22 15:53:41 -0400

    review: 417. Pacific Atlantic Water Flow [e]

--- problems/11-graphs/417-Pacific-Atlantic-Water-Flow/417-Pacific-Atlantic-Water-Flow.py
+++ problems/11-graphs/417-Pacific-Atlantic-Water-Flow/417-Pacific-Atlantic-Water-Flow.py
@@ -2,71 +2,51 @@
 https://leetcode.com/problems/pacific-atlantic-water-flow/
 '''
 
-last_solved     = "2026-07-21"
-revisit_in_days = 1
-times_reviewed  = 1
+last_solved     = "2026-07-22"
+revisit_in_days = 3
+times_reviewed  = 2
 difficulty      = "medium"
 topic_tags      = ["graphs", "dfs"]
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        def bfs(coord_set, coord_queue):
+            while coord_queue:
+                for _ in range(len(coord_queue)):
+                    row, col = coord_queue.popleft()
+
+                    directions = [
+                        (0, 1),
+                        (0, -1),
+                        (1, 0),
+                        (-1, 0)
+                    ]
+
+                    for row_adj, col_adj in directions:
+                        new_row, new_col = row + row_adj, col + col_adj
+
+                        if (
+                            0 <= new_row < M and
+                            0 <= new_col < N and
+                            (new_row, new_col) not in coord_set and
+                            heights[new_row][new_col] >= heights[row][col]
+                        ):
+                            coord_set.add((new_row, new_col))
+                            coord_queue.append((new_row, new_col))
+            
+            return coord_set
+
         M, N = len(heights), len(heights[0])
-        pacific_set, atlantic_set = set(), set()
-        pacific_queue, atlantic_queue = deque(), deque()
+        p_s, a_s = set(), set()
+        p_q, a_q = deque(), deque()
 
         for row in range(M):
             for col in range(N):
                 if row == 0 or col == 0:
-                    pacific_queue.append((row, col))
-                    pacific_set.add((row, col))
+                    p_s.add((row,col))
+                    p_q.append((row,col))
                 if row == M - 1 or col == N - 1:
-                    atlantic_queue.append((row, col))
-                    atlantic_set.add((row, col))
+                    a_s.add((row,col))
+                    a_q.append((row,col))
         
-        while pacific_queue:
-            for _ in range(len(pacific_queue)):
-                row, col = pacific_queue.popleft()
-
-                directions = [
-                    (1, 0),
-                    (-1, 0),
-                    (0, 1),
-                    (0, -1)
-                ]
-
-                for row_adj, col_adj in directions:
-                    new_row, new_col = row + row_adj, col + col_adj
-
-                    if (
-                        0 <= new_row < M and
-                        0 <= new_col < N and
-                        heights[new_row][new_col] >= heights[row][col] and
-                        (new_row, new_col) not in pacific_set
-                    ):
-                        pacific_queue.append((new_row, new_col))
-                        pacific_set.add((new_row, new_col))
-
-        while atlantic_queue:
-            for _ in range(len(atlantic_queue)):
-                row, col = atlantic_queue.popleft()
-
-                directions = [
-                    (1, 0),
-                    (-1, 0),
-                    (0, 1),
-                    (0, -1)
-                ]
-
-                for row_adj, col_adj in directions:
-                    new_row, new_col = row + row_adj, col + col_adj
-
-                    if (
-                        0 <= new_row < M and
-                        0 <= new_col < N and
-                        heights[new_row][new_col] >= heights[row][col] and
-                        (new_row, new_col) not in atlantic_set
-                    ):
-                        atlantic_queue.append((new_row, new_col))
-                        atlantic_set.add((new_row, new_col))
-        
-        return [[x, y] for x, y in atlantic_set.intersection(pacific_set)]
+        return [[row, col] for row, col in bfs(p_s, p_q).intersection(bfs(a_s, a_q))]
