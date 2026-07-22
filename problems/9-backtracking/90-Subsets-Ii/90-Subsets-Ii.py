db26d5b356965c07b19b57b369346f935f7623ea
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-07-21 20:11:27 -0400

    review(backtracking): 90 Subsets II — e (44 days)

--- problems/9-backtracking/90-Subsets-Ii/90-Subsets-Ii.py
+++ problems/9-backtracking/90-Subsets-Ii/90-Subsets-Ii.py
@@ -2,9 +2,9 @@
 https://leetcode.com/problems/subsets-ii/
 '''
 
-last_solved     = "2026-06-21"
-revisit_in_days = 30
-times_reviewed  = 6
+last_solved     = "2026-07-21"
+revisit_in_days = 44
+times_reviewed  = 7
 difficulty      = "medium"
 topic_tags      = ["backtracking", "recursion"]
 
