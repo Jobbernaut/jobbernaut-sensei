88935d5db4dc9840b00c9184106923e19ffc0734
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-07-24 18:00:53 -0400

    review(linked-list): 19. Remove Nth Node From End Of List — rated easy

--- problems/6-linked-list/19-Remove-Nth-Node-From-End-Of-List/19-Remove-Nth-Node-From-End-Of-List.py
+++ problems/6-linked-list/19-Remove-Nth-Node-From-End-Of-List/19-Remove-Nth-Node-From-End-Of-List.py
@@ -2,29 +2,27 @@
 https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 '''
 
-last_solved     = "2026-07-16"
-revisit_in_days = 7
-times_reviewed  = 6
+last_solved     = "2026-07-24"
+revisit_in_days = 44
+times_reviewed  = 7
 difficulty      = "medium"
 topic_tags      = ["linked-list", "two-pointers"]
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        left, right = head, head
+        dummy = ListNode()
+        dummy.next = head
 
-        N = n + 1
+        first, second = dummy, dummy
 
-        while right and N:
-            right = right.next
-            N -= 1
+        while n + 1:
+            second = second.next
+            n -= 1
         
-        if not right and N:
-            return head.next
+        while second:
+            first = first.next
+            second = second.next
         
-        while right:
-            left = left.next
-            right = right.next
-        
-        left.next = left.next.next
+        first.next = first.next.next
 
-        return head
+        return dummy.next
