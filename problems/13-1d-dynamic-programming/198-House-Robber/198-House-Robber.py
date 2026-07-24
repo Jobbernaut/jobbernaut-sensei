47ca3631ce4392a6eae03fcd3dd5c58bd9182021
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-07-24 18:01:04 -0400

    review(dynamic-programming): 198. House Robber — rated easy

--- problems/13-1d-dynamic-programming/198-House-Robber/198-House-Robber.py
+++ problems/13-1d-dynamic-programming/198-House-Robber/198-House-Robber.py
@@ -2,11 +2,11 @@
 https://leetcode.com/problems/house-robber/
 '''
 
-last_solved     = "2026-06-23"
-revisit_in_days = 30
+last_solved     = "2026-07-24"
+revisit_in_days = 45
 difficulty      = "medium"
 topic_tags      = ["dynamic-programming"]
-times_reviewed  = 5
+times_reviewed  = 6
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
