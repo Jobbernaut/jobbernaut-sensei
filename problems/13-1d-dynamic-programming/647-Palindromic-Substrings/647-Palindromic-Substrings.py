adcfe26ec6f5816d2b1929b927d3ceeeb636fd06
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-03 12:42:08 -0400

    review(dp): 647 Palindromic Substrings [e]

--- problems/13-1d-dynamic-programming/647-Palindromic-Substrings/647-Palindromic-Substrings.py
+++ problems/13-1d-dynamic-programming/647-Palindromic-Substrings/647-Palindromic-Substrings.py
@@ -2,26 +2,26 @@
 https://leetcode.com/problems/palindromic-substrings/
 '''
 
-last_solved     = "2026-08-02"
-revisit_in_days = 1
-times_reviewed  = 7
+last_solved     = "2026-08-03"
+revisit_in_days = 45
+times_reviewed  = 8
 difficulty      = "medium"
 topic_tags      = ["dynamic-programming", "strings"]
 
 class Solution:
     def countSubstrings(self, s: str) -> int:
         def expand(left, right):
-            cnt = 1 if right - left == 1 else 0
+            cnt = 0
 
-            while 0 <= left < right < len(s) and s[left] == s[right]:
-                cnt += 1
+            while 0 <= left <= right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
+                cnt += 1
             
             return cnt
         
-        total_cnt = 0
+        cnt = 0
         for idx in range(len(s)):
-            total_cnt += (expand(idx, idx + 1) + expand(idx - 1, idx + 1))
+            cnt += (expand(idx, idx) + expand(idx, idx + 1))
         
-        return total_cnt
+        return cnt
