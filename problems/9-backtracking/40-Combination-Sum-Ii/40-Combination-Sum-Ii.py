d3b81266debbbd959526a2ba328831095da48713
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-02 21:01:59 -0400

    review(backtracking): 40 Combination Sum II [s]

--- problems/9-backtracking/40-Combination-Sum-Ii/40-Combination-Sum-Ii.py
+++ problems/9-backtracking/40-Combination-Sum-Ii/40-Combination-Sum-Ii.py
@@ -2,9 +2,9 @@
 https://leetcode.com/problems/combination-sum-ii/
 '''
 
-last_solved     = "2026-07-01"
-revisit_in_days = 30
-times_reviewed  = 6
+last_solved     = "2026-08-02"
+revisit_in_days = 3
+times_reviewed  = 7
 difficulty      = "medium"
 topic_tags      = ["backtracking", "recursion"]
 
