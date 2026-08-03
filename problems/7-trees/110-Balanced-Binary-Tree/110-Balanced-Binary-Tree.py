c8c57bf2ac931fd1758e7cd51a85ac883746be19
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-02 21:01:59 -0400

    review(trees): 110 Balanced Binary Tree [s]

--- problems/7-trees/110-Balanced-Binary-Tree/110-Balanced-Binary-Tree.py
+++ problems/7-trees/110-Balanced-Binary-Tree/110-Balanced-Binary-Tree.py
@@ -2,11 +2,11 @@
 https://leetcode.com/problems/balanced-binary-tree/
 '''
 
-last_solved     = "2026-05-29"
-revisit_in_days = 64
+last_solved     = "2026-08-02"
+revisit_in_days = 1
 difficulty      = "easy"
 topic_tags      = ["trees"]
-times_reviewed  = 5
+times_reviewed  = 6
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
