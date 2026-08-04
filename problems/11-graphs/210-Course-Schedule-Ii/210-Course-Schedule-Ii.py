529b6fcb3a16596dff6aa52b88145a1968148dff
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-04 11:50:02 -0400

    feat(graphs): add 210 Course Schedule II [e]

---
+++ problems/11-graphs/210-Course-Schedule-Ii/210-Course-Schedule-Ii.py
@@ -0,0 +1,43 @@
+'''
+https://leetcode.com/problems/course-schedule-ii/
+'''
+
+last_solved     = "2026-08-04"
+revisit_in_days = 1
+times_reviewed  = 1
+difficulty      = "medium"
+topic_tags      = ["graphs", "topological-sort", "dfs"]
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj_lst = defaultdict(set)
+        indegree_lst = [0] * numCourses
+
+        for course, prereq in prerequisites:
+            adj_lst[prereq].add(course)
+            indegree_lst[course] += 1
+        
+        output = []
+
+        q = deque()
+
+        for course_no, indegree in enumerate(indegree_lst):
+            if indegree == 0:
+                q.append(course_no)
+        
+        while q:
+            for _ in range(len(q)):
+                curr = q.popleft()
+
+                output.append(curr)
+
+                for each_course in adj_lst[curr]:
+                    indegree_lst[each_course] -= 1
+
+                    if indegree_lst[each_course] == 0:
+                        q.append(each_course)
+
+        if len(output) < numCourses:
+            return []
+        
+        return output
--- problems/NEETCODE150.md
+++ problems/NEETCODE150.md
@@ -1,6 +1,6 @@
 # NeetCode 150 Progress Tracker
 
-Total Completed: **80 / 150**
+Total Completed: **81 / 150**
 
 ## 1. Arrays & Hashing
 - [x] 217. Contains Duplicate (Easy)
@@ -109,7 +109,7 @@ Total Completed: **80 / 150**
 - [ ] 130. Surrounded Regions (Medium)
 - [x] 994. Rotting Oranges (Medium)
 - [x] 286. Walls and Gates (Medium)
-- [ ] 210. Course Schedule II (Medium)
+- [x] 210. Course Schedule II (Medium)
 - [ ] 684. Redundant Connection (Medium)
 - [ ] 323. Number of Connected Components in an Undirected Graph (Medium)
 - [ ] 261. Graph Valid Tree (Medium)
