68cfa9a5a03d0c63128d59d141f3ed81e1ae9061
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-04 10:57:34 -0400

    feat(1d-dp): add 91 Decode Ways [h]

---
+++ problems/13-1d-dynamic-programming/91-Decode-Ways/91-Decode-Ways.py
@@ -0,0 +1,21 @@
+'''
+https://leetcode.com/problems/decode-ways/
+'''
+
+last_solved     = "2026-08-04"
+revisit_in_days = 1
+times_reviewed  = 1
+difficulty      = "medium"
+topic_tags      = ["dynamic-programming"]
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        prev_2 = 1
+        prev_1 = 0 if s[0] == "0" else 1
+
+        for idx in range(1, len(s)):
+            curr = (prev_2 if 10 <= int(s[idx-1:idx+1]) <= 26 else 0) + (prev_1 if 1 <= int(s[idx]) <= 9 else 0)
+            prev_2 = prev_1
+            prev_1 = curr
+        
+        return prev_1
