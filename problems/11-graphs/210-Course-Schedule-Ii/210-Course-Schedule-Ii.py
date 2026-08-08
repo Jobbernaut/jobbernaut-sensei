70eec0a013d809e02e29204515f365427c8c9942
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-08 11:15:41 -0400

    chore(srs): update 210 Course Schedule II schedule after review

--- problems/11-graphs/210-Course-Schedule-Ii/210-Course-Schedule-Ii.py
+++ problems/11-graphs/210-Course-Schedule-Ii/210-Course-Schedule-Ii.py
@@ -2,9 +2,9 @@
 https://leetcode.com/problems/course-schedule-ii/
 '''
 
-last_solved     = "2026-08-05"
-revisit_in_days = 3
-times_reviewed  = 2
+last_solved     = "2026-08-08"
+revisit_in_days = 7
+times_reviewed  = 3
 difficulty      = "medium"
 topic_tags      = ["graphs", "topological-sort", "dfs"]
 
