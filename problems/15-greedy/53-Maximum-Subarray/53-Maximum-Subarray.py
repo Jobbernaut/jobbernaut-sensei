dab7027f22758be2a1725ed4096268b16f951444
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-11 11:58:54 -0400

    chore(srs): update 53 Maximum Subarray schedule after review

--- problems/15-greedy/53-Maximum-Subarray/53-Maximum-Subarray.py
+++ problems/15-greedy/53-Maximum-Subarray/53-Maximum-Subarray.py
@@ -9,6 +9,16 @@ difficulty      = "medium"
 topic_tags      = ["dynamic-programming", "greedy"]
 
 class Solution:
-    def solve(self) -> None:
-        # TODO: implement
-        pass
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_max = 0
+        global_max = min(nums)
+
+        for n in nums:
+            curr_max += n
+            
+            global_max = max(global_max, curr_max)
+
+            if curr_max < 0:
+                curr_max = 0
+        
+        return global_max
