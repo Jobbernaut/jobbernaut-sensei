e6c9882b3318a86df81e6a2559d8108acb238c7f
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-12 11:55:51 -0400

    feat(intervals): add 57 Insert Interval [h]

---
+++ problems/16-intervals/57-Insert-Interval/57-Insert-Interval.py
@@ -0,0 +1,32 @@
+'''
+https://leetcode.com/problems/insert-interval/
+'''
+
+last_solved     = "2026-08-12"
+revisit_in_days = 1
+times_reviewed  = 1
+difficulty      = "medium"
+topic_tags      = ["array"]
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        output = []
+
+        new_lower, new_upper = newInterval
+
+        for idx, interval in enumerate(intervals):
+            lower, upper = interval
+
+            if max(lower, new_lower) <= min(upper, new_upper):
+                new_lower = min(lower, new_lower)
+                new_upper = max(upper, new_upper)
+            elif new_upper < lower:
+                output.append([new_lower, new_upper])
+                output.extend(intervals[idx:])
+                return output
+            else:
+                output.append([lower, upper])
+        
+        output.append([new_lower, new_upper])
+
+        return output
