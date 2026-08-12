c13666a3c722e9d6a7a96b620f95861a3573d0a5
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-12 11:55:48 -0400

    feat(greedy): add 45 Jump Game II [h]

---
+++ problems/15-greedy/45-Jump-Game-Ii/45-Jump-Game-Ii.py
@@ -0,0 +1,24 @@
+'''
+https://leetcode.com/problems/jump-game-ii/
+'''
+
+last_solved     = "2026-08-12"
+revisit_in_days = 1
+times_reviewed  = 0
+difficulty      = "medium"
+topic_tags      = ["array", "dynamic-programming", "greedy"]
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        current_end = 0
+        farthest = nums[0]
+        jumps = 0
+
+        for idx in range(0, len(nums) - 1):
+            farthest = max(farthest, idx + nums[idx])
+
+            if idx == current_end:
+                jumps += 1
+                current_end = farthest
+        
+        return jumps
