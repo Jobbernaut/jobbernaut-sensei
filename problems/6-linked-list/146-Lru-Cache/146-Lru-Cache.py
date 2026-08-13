d7626ebedf3f4566b497752bc91c86adcdc702b0
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-13 18:40:03 -0400

    chore(srs): update 146 LRU Cache schedule after review [g]

--- problems/6-linked-list/146-Lru-Cache/146-Lru-Cache.py
+++ problems/6-linked-list/146-Lru-Cache/146-Lru-Cache.py
@@ -2,9 +2,9 @@
 https://leetcode.com/problems/lru-cache/
 '''
 
-last_solved     = "2026-08-10"
-revisit_in_days = 3
-times_reviewed  = 2
+last_solved     = "2026-08-13"
+revisit_in_days = 7
+times_reviewed  = 3
 difficulty      = "medium"
 topic_tags      = ["linked-list", "hash-map", "design"]
 
