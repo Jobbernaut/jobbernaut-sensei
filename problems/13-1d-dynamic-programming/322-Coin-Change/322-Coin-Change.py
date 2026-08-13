df283a2d9203c269f3082d87defa5250357da414
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-13 18:40:08 -0400

    feat(1d-dynamic-programming): add solution for 322 Coin Change [e]

--- problems/13-1d-dynamic-programming/322-Coin-Change/322-Coin-Change.py
+++ problems/13-1d-dynamic-programming/322-Coin-Change/322-Coin-Change.py
@@ -2,13 +2,27 @@
 https://leetcode.com/problems/coin-change/
 '''
 
-last_solved     = "2026-08-10"
-revisit_in_days = 3
-times_reviewed  = 2
+last_solved     = "2026-08-13"
+revisit_in_days = 7
+times_reviewed  = 3
 difficulty      = "medium"
 topic_tags      = ["dynamic-programming"]
 
 class Solution:
-    def solve(self) -> None:
-        # TODO: implement
-        pass
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for amt in range(1, amount + 1):
+            min_coins = float('inf')
+
+            for d in coins:
+                if d <= amt:
+                    min_coins = min(min_coins, 1 + dp[amt - d])
+            
+            dp[amt] = min_coins
+        
+        if isinf(dp[amount]):
+            return -1
+        
+        return dp[amount]
