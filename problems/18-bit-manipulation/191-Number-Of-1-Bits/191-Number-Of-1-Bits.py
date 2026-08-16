0d3aa1a30b5debd46b94d7821fbee3be02a9bab7
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-16 14:11:46 -0400

    feat(bit-manipulation): solve 191 Number of 1 Bits [g]

---
+++ problems/18-bit-manipulation/191-Number-Of-1-Bits/191-Number-Of-1-Bits.py
@@ -0,0 +1,14 @@
+'''
+https://leetcode.com/problems/number-of-1-bits/
+'''
+
+last_solved     = "2026-08-16"
+revisit_in_days = 1
+times_reviewed  = 1
+difficulty      = "easy"
+topic_tags      = ["divide-and-conquer", "bit-manipulation"]
+
+class Solution:
+    def solve(self) -> None:
+        # TODO: implement
+        pass
