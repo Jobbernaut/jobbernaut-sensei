f9bd944310e255fbcbc56039263e76476d7dab0e
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-17 13:34:07 -0400

    feat(intervals): solve 56 Merge Intervals [h]

---
+++ problems/16-intervals/56-Merge-Intervals/56-Merge-Intervals.py
@@ -0,0 +1,28 @@
+'''
+https://leetcode.com/problems/merge-intervals/
+'''
+
+last_solved     = "2026-08-17"
+revisit_in_days = 1
+times_reviewed  = 1
+difficulty      = "medium"
+topic_tags      = ["array", "sorting", "quicksort"]
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        
+        output = []
+
+        curr_start, curr_end = intervals[0][0], intervals[0][1]
+        for next_start, next_end in intervals[1:]:
+            if max(curr_start, next_start) <= min(curr_end, next_end):
+                curr_start = min(curr_start, next_start)
+                curr_end = max(curr_end, next_end)
+            else:
+                output.append([curr_start, curr_end])
+                curr_start, curr_end = next_start, next_end
+        
+        output.append([curr_start, curr_end])
+
+        return output
