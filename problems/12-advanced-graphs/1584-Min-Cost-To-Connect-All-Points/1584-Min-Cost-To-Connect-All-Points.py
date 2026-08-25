94cdcf10b01b7df5289d544b12bfef89193bd195
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-24 20:49:19 -0400

    feat: add Prim's MST solution for 1584 Min Cost To Connect All Points

--- problems/12-advanced-graphs/1584-Min-Cost-To-Connect-All-Points/1584-Min-Cost-To-Connect-All-Points.py
+++ problems/12-advanced-graphs/1584-Min-Cost-To-Connect-All-Points/1584-Min-Cost-To-Connect-All-Points.py
@@ -2,13 +2,33 @@
 https://leetcode.com/problems/min-cost-to-connect-all-points/
 '''
 
-last_solved     = "2026-08-20"
-revisit_in_days = 1
-times_reviewed  = 1
+last_solved     = "2026-08-22"
+revisit_in_days = 3
+times_reviewed  = 2
 difficulty      = "medium"
 topic_tags      = ["array", "union-find", "graph", "minimum-spanning-tree", "prims-algorithm", "kruskals-algorithm", "boruvkas-algorithm"]
 
 class Solution:
-    def solve(self) -> None:
-        # TODO: implement
-        pass
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        min_cost = 0
+        visited = set()
+        heap = []
+
+        heapq.heappush(heap, [0, points[0]])
+
+        while heap:
+            for _ in range(len(heap)):
+                d, point = heapq.heappop(heap)
+
+                x, y = point
+
+                if (x, y) not in visited:
+                    visited.add((x, y))
+
+                    min_cost += d
+
+                    for n_x, n_y in points:
+                        if (n_x, n_y) not in visited:
+                            heapq.heappush(heap, [abs(x - n_x) + abs(y - n_y), [n_x, n_y]])
+        
+        return min_cost
