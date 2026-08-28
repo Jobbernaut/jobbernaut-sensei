052e0f240fc1ae51c28eefd74421e3f9b2da527f
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-08-28 17:42:16 -0400

    review(2d-dynamic-programming): review session 2026-08-28 for 494 Target Sum

--- problems/14-2d-dynamic-programming/494-Target-Sum/494-Target-Sum.py
+++ problems/14-2d-dynamic-programming/494-Target-Sum/494-Target-Sum.py
@@ -2,9 +2,9 @@
 https://leetcode.com/problems/target-sum/
 '''
 
-last_solved     = "2026-08-24"
-revisit_in_days = 3
-times_reviewed  = 2
+last_solved     = "2026-08-28"
+revisit_in_days = 7
+times_reviewed  = 3
 difficulty      = "medium"
 topic_tags      = ["array", "dynamic-programming", "backtracking", "knapsack-problem", "0-1-knapsack"]
 
